68f3d20e963280c6890779bda38143d47149331e
jsche <jsche@172.16.52.255>
2018-03-19 12:58:37 +0100

    df

--- src/main/java/GA2/ejb/LeihvertragBean.java
+++ src/main/java/GA2/ejb/LeihvertragBean.java
@@ -5,6 +5,8 @@
  */
 package GA2.ejb;
 
+import GA2.jpa.Fahrzeug;
+import GA2.jpa.Kunde;
 import GA2.jpa.Leihvertrag;
 import java.util.Date;
 import java.util.List;
@@ -21,13 +23,22 @@ public class LeihvertragBean extends EntityBean<Leihvertrag, Long> {
         super(Leihvertrag.class);
     }
     
-    public List<Leihvertrag> findByRentBetween(Date rentStart, Date rentEnde) {
+    public List<Leihvertrag> findByRentBetween(Date rentStart, Date rentEnde, Fahrzeug fahrzeug) {
         return em.createQuery("SELECT l FROM Leihvertrag l"
-                            + "WHERE (l.beginnDatum > :rentStart AND l.beginnDatum > :rentEnde)"
-                            + "OR (l.endeDatum < :rentStart AND l.endeDatum < :rentEnde"
+                            + "WHERE ((l.beginnDatum > :rentStart AND l.beginnDatum > :rentEnde)"
+                            + "OR (l.endeDatum < :rentStart AND l.endeDatum < :rentEnde))"
+                            + "AND l.fahzeugId.id == fahrzeug.id"
                             + "ORDER BY l.beginnDatum, l.fahrzeugID.modell")
                             .setParameter("rentStart", rentStart)
                             .setParameter("rentEnde", rentEnde)
+                            .getResultList();
+    }
+    
+    public List<Leihvertrag> findAllByKunde (Kunde kunde) {
+        return em.createQuery("SELECT l FROM Leihvertrag l"
+                            + "WHERE l.kundenId.id == kunde.id"
+                            + "ORDER BY l.startDatum")
+                            .setParameter("kunde", kunde)
                             .getResultList();
     }
 }
